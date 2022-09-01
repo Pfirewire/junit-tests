355d0c70e3c00350475ea11c4b1a507011082445
Stephen Pflug <stephen@pflug.us>
2022-09-01 08:19:42 -0500

    moved from copy

---
+++ src/main/java/Student.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class Student {
+
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student() {
+        id = 0;
+        name = "";
+        grades = new ArrayList<>();
+    }
+
+    public Student(long id, String name) {
+        this.id = id;
+        this.name = name;
+        grades = new ArrayList<>();
+    }
+    // returns the student's id
+    public long getId(){
+        return id;
+    }
+
+    // returns the student's name
+    public String getName(){
+        return name;
+    }
+
+    // adds the given grade to the grades list
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    // returns the list of grades
+    public ArrayList<Integer> getGrades(){
+        return grades;
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        Integer gradeTotal = 0;
+        for (Integer grade : grades) {
+            gradeTotal += grade;
+        }
+        return gradeTotal.doubleValue() / grades.size();
+    }
+}
---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,76 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class StudentTest {
+
+    private Student emptyStudent;
+    private Student testStudent;
+    private ArrayList<Integer> emptyGrades;
+    private ArrayList<Integer> testGrades;
+
+    @Before
+    public void setUp() {
+        emptyStudent = new Student();
+        emptyGrades = new ArrayList<>();
+
+        testStudent = new Student(12345, "John Doe");
+        testStudent.addGrade(80);
+        testStudent.addGrade(100);
+        testStudent.addGrade(50);
+        testStudent.addGrade(98);
+        testStudent.addGrade(89);
+
+        testGrades = new ArrayList<>();
+        testGrades.add(80);
+        testGrades.add(100);
+        testGrades.add(50);
+        testGrades.add(98);
+        testGrades.add(89);
+    }
+    @Test
+    public void testGetID() {
+        assertEquals(0, emptyStudent.getId());
+        assertEquals(12345, testStudent.getId());
+        Student otherTestStudent = new Student(23456, "Name");
+        assertEquals(23456, otherTestStudent.getId());
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals("", emptyStudent.getName());
+        assertEquals("John Doe", testStudent.getName());
+        Student otherTestStudent = new Student(23456, "Name");
+        assertEquals("Name", otherTestStudent.getName());
+    }
+
+    @Test
+    public void testGetGrades() {
+        assertEquals(emptyGrades, emptyStudent.getGrades());
+        assertEquals(testGrades, testStudent.getGrades());
+    }
+
+    @Test
+    public void testAddGrades() {
+        testStudent.addGrade(50);
+        testGrades.add(50);
+        assertEquals(testGrades, testStudent.getGrades());
+        testStudent.addGrade(87);
+        testGrades.add(87);
+        assertEquals(testGrades, testStudent.getGrades());
+    }
+
+    @Test
+    public void testGetGradeAverage() {
+        Integer totalGrades = 0;
+        double gradeAverage;
+        for(Integer grade : testGrades) {
+            totalGrades += grade;
+        }
+        gradeAverage = totalGrades.doubleValue() / testGrades.size();
+        assertEquals(gradeAverage, testStudent.getGradeAverage(), 0.1);
+    }
+}
