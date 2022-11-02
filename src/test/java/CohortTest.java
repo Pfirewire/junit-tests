c24a8c4494e11c7a6f1e2e52668b9853346cf0d4
Stephen Pflug <stephen@pflug.us>
2022-11-02 14:19:36 -0500

    finished up part two

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class CohortTest {
@@ -9,6 +11,7 @@ public class CohortTest {
     private Cohort testCohort;
     private Student student1;
     private Student student2;
+    private Student student3;
 
     @Before
     public void setUp() {
@@ -26,16 +29,35 @@ public class CohortTest {
         student2.addGrade(82);
         student2.addGrade(77);
         student2.addGrade(88);
+        student3 = new Student(12347, "Joan Doo");
+        student3.addGrade(44);
+        student3.addGrade(98);
+        student3.addGrade(59);
+        student3.addGrade(66);
+        student3.addGrade(33);
         testCohort.addStudent(student1);
         testCohort.addStudent(student2);
+        testCohort.addStudent(student3);
     }
 
     @Test
     public void testAddStudent() {
         assertTrue(emptyCohort.isEmpty());
-        emptyCohort.addStudent(student1);
-        emptyCohort.addStudent(student2);
-        assertFalse(emptyCohort.isEmpty());
+        assertFalse(testCohort.isEmpty());
+    }
+
+    @Test
+    public void testGetStudents() {
+        ArrayList<Student> studentList = new ArrayList<>(List.of(student1, student2, student3));
+        assertEquals(studentList.get(0), testCohort.getStudents().get(0));
+        assertEquals(studentList.get(1), testCohort.getStudents().get(1));
+        assertEquals(studentList.get(2), testCohort.getStudents().get(2));
+    }
+
+    @Test
+    public void testGetCohortAverage() {
+        double cohortAverage = (student1.getGradeAverage() + student2.getGradeAverage() + student3.getGradeAverage()) / 3;
+        assertEquals(cohortAverage, testCohort.getCohortAverage(), 0.0001);
     }
 
 }
